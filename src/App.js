bfac899c06ce45e91fa1544fd9686df1bbdf25d3
Chadivesreenath <sreenath.chadive@gmail.com>
2022-12-15 22:52:21 +0530

    code push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import NewsCard from './Components/NewsCard/NewsCard';
+import NewsContent from './Components/NewsContent/NewsContent';
 
 function App() {
   return (
@@ -6,6 +9,9 @@ function App() {
     <center className="App">
       Inshorts Application
     </center>
+    <NewsCard />
+    <NewsContent />
+    <Footer />
     </>
   );
 }
---
+++ src/Components/NewsContent/NewsContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./NewsContent.css"
+
+
+const NewsContent = () => {
+  return (
+    <div>NewsContent</div>
+  )
+}
+
+export default NewsContent
