1b066ea665037d90e15dcfb834b6c17d917ad923
Chadivesreenath <sreenath.chadive@gmail.com>
2022-12-31 21:22:59 +0530

    updated links

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,19 +10,20 @@ const Footer = () => {
                 Inshorts Clone made by - {" "} <a href="https://www.linkedin.com/in/sreenath-chadive-542762192/">Chadive Sreenath </a>
             </span>
 
-            <hr style={{ width: "90%" }} />
+            <hr style={{ width: "96%" }} />
 
             <div className="iconContainer">
-                     {/* eslint-disable-next-line */}
-                <a href="https://www.instagram.com/sreenath_s6/" target="_blank">
-                    <i className="fab fa-instagram-square fa-2x"></i>
-                </a>
+                {/* eslint-disable-next-line */}
                 <a href="https://www.linkedin.com/in/sreenath-chadive-542762192/" target="__blank">
                     <i className="fab fa-linkedin fa-2x"></i>
+                </a>
+                <a href="https://www.instagram.com/sreenath_s6/" target="_blank">
+                    <i className="fab fa-instagram-square fa-2x"></i>
                 </a>
                 <a href="https://github.com/ChadiveSreenath" target="_blank" rel="noopener noreferrer">
                     <i className="fas fa-link fa-2x"></i>
                 </a>
+
             </div>
 
         </div>
